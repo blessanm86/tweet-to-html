e09d0df89de7f82b55556c5a6d5428a330e02444
Blessan Mathew <blessenm@gmail.com>
2015-10-21 21:37:27 +0530

    Test cases are now run by Mocha. Added tests for asynchronous mode.

--- test/index.js
+++ test/index.js
@@ -10,24 +10,89 @@ var tweetWithLinksPicMention = require('./tweets/tweet-with-links-pic-mention');
 var tweetWithLinksPicMentionExpected = require('./tweets/tweet-with-links-pic-mention-expected');
 
 var parse = TweetToHTML.parse;
+var multipleTweets = [tweetWithVideo, tweetWithEmoji, tweetWithHashtag, tweetWithLinksPicMention];
+var multipleTweetsExpected = [tweetWithVideoExpected, tweetWithEmojiExpected, tweetWithHashtagExpected, tweetWithLinksPicMentionExpected];
 
-assert.strictEqual(
-  parse(tweetWithVideo).text,
-  tweetWithVideoExpected,
-  'Synchonous parsing of video in tweet is successful'
-);
-assert.strictEqual(
-  parse(tweetWithEmoji).text,
-  tweetWithEmojiExpected,
-  'Synchonous parsing of emoji in tweet is successful'
-);
-assert.strictEqual(
-  parse(tweetWithHashtag).text,
-  tweetWithHashtagExpected,
-  'Synchonous parsing of hashtag in tweet is successful'
-);
-assert.strictEqual(
-  parse(tweetWithLinksPicMention).text,
-  tweetWithLinksPicMentionExpected,
-  'Synchonous parsing of links, pic, and mention in tweet is successful'
-);
+
+
+describe("Synchronous and Single Tweet Test Suite", function() {
+  it('Synchonous parsing of video in tweet is successful', function() {
+    assert.strictEqual(parse(tweetWithVideo).text, tweetWithVideoExpected);
+  });
+
+  it('Synchonous parsing of emoji in tweet is successful', function() {
+    assert.strictEqual(parse(tweetWithEmoji).text, tweetWithEmojiExpected);
+  });
+
+  it('Synchonous parsing of hashtag in tweet is successful', function() {
+    assert.strictEqual(parse(tweetWithHashtag).text, tweetWithHashtagExpected);
+  });
+
+  it('Synchonous parsing of links, pic, and mention in tweet is successful', function() {
+    assert.strictEqual(parse(tweetWithLinksPicMention).text, tweetWithLinksPicMentionExpected);
+  });
+});
+
+
+
+describe("Synchronous and Multiple Tweets Test Suite", function() {
+  it('Synchonous parsing of videos, emojis, hashtags, links, pics, and mentions in multiple tweets is successful', function() {
+    parse(multipleTweets, (tweets) => {
+      tweets.forEach((tweet, index) => {
+        assert.strictEqual(
+          tweet.text,
+          multipleTweetsExpected[index]
+        );
+      });
+    })
+  });
+});
+
+
+
+describe("Asynchronous and Single Tweet Test Suite", function() {
+  it('Asynchonous parsing of video in tweet is successful', function(done) {
+    parse(tweetWithVideo, function(tweetObj) {
+      assert.strictEqual(tweetObj.text, tweetWithVideoExpected);
+      done();
+    });
+  });
+
+  it('Asynchonous parsing of emoji in tweet is successful', function(done) {
+    parse(tweetWithEmoji, function(tweetObj) {
+      assert.strictEqual(tweetObj.text, tweetWithEmojiExpected);
+      done();
+    });
+  });
+
+  it('Asynchonous parsing of hashtag in tweet is successful', function(done) {
+    parse(tweetWithHashtag, function(tweetObj) {
+      assert.strictEqual(tweetObj.text, tweetWithHashtagExpected);
+      done();
+    });
+  });
+
+  it('Asynchonous parsing of links, pic, and mention in tweet is successful', function(done) {
+    parse(tweetWithLinksPicMention, function(tweetObj) {
+      assert.strictEqual(tweetObj.text, tweetWithLinksPicMentionExpected);
+      done();
+    });
+  });
+});
+
+
+
+describe("Asynchronous and Multiple Tweets Test Suite", function() {
+  it('Asynchonous parsing of videos, emojis, hashtags, links, pics, and mentions in multiple tweets is successful', function(done) {
+    parse(multipleTweets, (tweets) => {
+      tweets.forEach((tweet, index) => {
+        assert.strictEqual(
+          tweet.text,
+          multipleTweetsExpected[index]
+        );
+      });
+
+      done();
+    });
+  });
+});
