4950f2cfb6c285a579fc8b4a436b08bf708a7600
Blessan Mathew <blessenm@gmail.com>
2015-11-12 20:45:39 +0530

    Updated demo to show rendered output and added demo showing quoted tweet.

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -1,6 +1,7 @@
 var tweetToHTML = require('../../');
 var tweetWithVideo = require('../../test/tweets/tweet-with-video');
 var tweetWithEmoji = require('../../test/tweets/tweet-with-emoji');
+var tweetWithQuotedTweet = require('../../test/tweets/tweet-with-quoted-tweet');
 
 function encodeHTML(html) {
   var tagsToReplace = {
@@ -18,9 +19,16 @@ function encodeHTML(html) {
 
 document.getElementById('input-1').innerHTML = JSON.stringify(tweetWithVideo, null, 2);
 document.getElementById('input-2').innerHTML = JSON.stringify(tweetWithEmoji, null, 2);
+document.getElementById('input-3').innerHTML = JSON.stringify(tweetWithQuotedTweet, null, 2);
 
 var html1 = tweetToHTML.parse(tweetWithVideo).html;
 var html2 = tweetToHTML.parse(tweetWithEmoji).html;
+var html3 = tweetToHTML.parse(tweetWithQuotedTweet).html;
 
 document.getElementById('output-1').innerHTML = encodeHTML(style_html(html1));
 document.getElementById('output-2').innerHTML = encodeHTML(style_html(html2));
+document.getElementById('output-3').innerHTML = encodeHTML(style_html(html3));
+
+document.getElementById('render-1').innerHTML = html1;
+document.getElementById('render-2').innerHTML = html2;
+document.getElementById('render-3').innerHTML = html3;
