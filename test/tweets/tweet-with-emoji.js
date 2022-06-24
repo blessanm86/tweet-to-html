0f759c80d8e76bc193fb0c26741f7bdc1fa7c822
Mehul Kar <mehul.kar@gmail.com>
2022-06-24 13:38:58 -0700

    Run eslint autofixer to fix formatting issues

--- test/tweets/tweet-with-emoji.js
+++ test/tweets/tweet-with-emoji.js
@@ -1,3 +1,102 @@
 //Tweet url -> https://twitter.com/Support/status/451399393850052610
 
-module.exports = {"created_at":"Wed Apr 02 16:42:56 +0000 2014","id":451399393850052610,"id_str":"451399393850052610","text":"\ud83d\ude0d Twitter users love tweeting emoji from their phones \u2013 now you can view these emoji on web! #emojiparty \ud83c\udf89 \ud83d\ude1c \ud83d\udc6f \ud83c\udf7b \ud83c\udf88 \ud83c\udfa4 \ud83c\udfae \ud83d\ude80 \ud83c\udf09 \u2728","source":"\u003ca href=\"http:\/\/twitter.com\/download\/iphone\" rel=\"nofollow\"\u003eTwitter for iPhone\u003c\/a\u003e","truncated":false,"in_reply_to_status_id":null,"in_reply_to_status_id_str":null,"in_reply_to_user_id":null,"in_reply_to_user_id_str":null,"in_reply_to_screen_name":null,"user":{"id":17874544,"id_str":"17874544","name":"Twitter Support","screen_name":"Support","location":"Twitter HQ","description":"We Tweet tips and tricks to help you boost your Twitter skills and keep your account secure. For detailed help, visit http:\/\/t.co\/qq1HEzdMA2.","url":"http:\/\/t.co\/Vk1NkwU8qP","entities":{"url":{"urls":[{"url":"http:\/\/t.co\/Vk1NkwU8qP","expanded_url":"http:\/\/support.twitter.com","display_url":"support.twitter.com","indices":[0,22]}]},"description":{"urls":[{"url":"http:\/\/t.co\/qq1HEzdMA2","expanded_url":"http:\/\/support.twitter.com","display_url":"support.twitter.com","indices":[118,140]}]}},"protected":false,"followers_count":4395480,"friends_count":31,"listed_count":13709,"created_at":"Thu Dec 04 18:51:57 +0000 2008","favourites_count":293,"utc_offset":-25200,"time_zone":"Pacific Time (US & Canada)","geo_enabled":true,"verified":true,"statuses_count":13243,"lang":"en","contributors_enabled":false,"is_translator":false,"is_translation_enabled":false,"profile_background_color":"C0DEED","profile_background_image_url":"http:\/\/pbs.twimg.com\/profile_background_images\/656929496\/y6jd4l68p18hrm52f0ez.png","profile_background_image_url_https":"https:\/\/pbs.twimg.com\/profile_background_images\/656929496\/y6jd4l68p18hrm52f0ez.png","profile_background_tile":true,"profile_image_url":"http:\/\/pbs.twimg.com\/profile_images\/533026436190175232\/1i65YBa7_normal.png","profile_image_url_https":"https:\/\/pbs.twimg.com\/profile_images\/533026436190175232\/1i65YBa7_normal.png","profile_banner_url":"https:\/\/pbs.twimg.com\/profile_banners\/17874544\/1347394418","profile_link_color":"0084B4","profile_sidebar_border_color":"C0DEED","profile_sidebar_fill_color":"DDEEF6","profile_text_color":"333333","profile_use_background_image":true,"has_extended_profile":false,"default_profile":false,"default_profile_image":false,"following":false,"follow_request_sent":false,"notifications":false},"geo":null,"coordinates":null,"place":null,"contributors":null,"is_quote_status":false,"retweet_count":5109,"favorite_count":3069,"entities":{"hashtags":[{"text":"emojiparty","indices":[93,104]}],"symbols":[],"user_mentions":[],"urls":[]},"favorited":false,"retweeted":false,"lang":"en"};
+module.exports = {
+  created_at: 'Wed Apr 02 16:42:56 +0000 2014',
+  id: 451399393850052610,
+  id_str: '451399393850052610',
+  text: '\ud83d\ude0d Twitter users love tweeting emoji from their phones \u2013 now you can view these emoji on web! #emojiparty \ud83c\udf89 \ud83d\ude1c \ud83d\udc6f \ud83c\udf7b \ud83c\udf88 \ud83c\udfa4 \ud83c\udfae \ud83d\ude80 \ud83c\udf09 \u2728',
+  source:
+    '\u003ca href="http://twitter.com/download/iphone" rel="nofollow"\u003eTwitter for iPhone\u003c/a\u003e',
+  truncated: false,
+  in_reply_to_status_id: null,
+  in_reply_to_status_id_str: null,
+  in_reply_to_user_id: null,
+  in_reply_to_user_id_str: null,
+  in_reply_to_screen_name: null,
+  user: {
+    id: 17874544,
+    id_str: '17874544',
+    name: 'Twitter Support',
+    screen_name: 'Support',
+    location: 'Twitter HQ',
+    description:
+      'We Tweet tips and tricks to help you boost your Twitter skills and keep your account secure. For detailed help, visit http://t.co/qq1HEzdMA2.',
+    url: 'http://t.co/Vk1NkwU8qP',
+    entities: {
+      url: {
+        urls: [
+          {
+            url: 'http://t.co/Vk1NkwU8qP',
+            expanded_url: 'http://support.twitter.com',
+            display_url: 'support.twitter.com',
+            indices: [0, 22],
+          },
+        ],
+      },
+      description: {
+        urls: [
+          {
+            url: 'http://t.co/qq1HEzdMA2',
+            expanded_url: 'http://support.twitter.com',
+            display_url: 'support.twitter.com',
+            indices: [118, 140],
+          },
+        ],
+      },
+    },
+    protected: false,
+    followers_count: 4395480,
+    friends_count: 31,
+    listed_count: 13709,
+    created_at: 'Thu Dec 04 18:51:57 +0000 2008',
+    favourites_count: 293,
+    utc_offset: -25200,
+    time_zone: 'Pacific Time (US & Canada)',
+    geo_enabled: true,
+    verified: true,
+    statuses_count: 13243,
+    lang: 'en',
+    contributors_enabled: false,
+    is_translator: false,
+    is_translation_enabled: false,
+    profile_background_color: 'C0DEED',
+    profile_background_image_url:
+      'http://pbs.twimg.com/profile_background_images/656929496/y6jd4l68p18hrm52f0ez.png',
+    profile_background_image_url_https:
+      'https://pbs.twimg.com/profile_background_images/656929496/y6jd4l68p18hrm52f0ez.png',
+    profile_background_tile: true,
+    profile_image_url:
+      'http://pbs.twimg.com/profile_images/533026436190175232/1i65YBa7_normal.png',
+    profile_image_url_https:
+      'https://pbs.twimg.com/profile_images/533026436190175232/1i65YBa7_normal.png',
+    profile_banner_url:
+      'https://pbs.twimg.com/profile_banners/17874544/1347394418',
+    profile_link_color: '0084B4',
+    profile_sidebar_border_color: 'C0DEED',
+    profile_sidebar_fill_color: 'DDEEF6',
+    profile_text_color: '333333',
+    profile_use_background_image: true,
+    has_extended_profile: false,
+    default_profile: false,
+    default_profile_image: false,
+    following: false,
+    follow_request_sent: false,
+    notifications: false,
+  },
+  geo: null,
+  coordinates: null,
+  place: null,
+  contributors: null,
+  is_quote_status: false,
+  retweet_count: 5109,
+  favorite_count: 3069,
+  entities: {
+    hashtags: [{ text: 'emojiparty', indices: [93, 104] }],
+    symbols: [],
+    user_mentions: [],
+    urls: [],
+  },
+  favorited: false,
+  retweeted: false,
+  lang: 'en',
+};
--- test/tweets/tweet-with-hashtag.js
+++ test/tweets/tweet-with-hashtag.js
@@ -1,3 +1,93 @@
 //Tweet url -> https://twitter.com/derickbailey/status/656469398526361600
 
-module.exports = {"created_at":"Tue Oct 20 13:57:55 +0000 2015","id":656469398526361600,"id_str":"656469398526361600","text":"and in today's #EveryDaySexism, my wife can't get vehicle registration sticker because her last name is different than mine \ud83d\ude20\ud83d\ude2c","source":"\u003ca href=\"http:\/\/twitter.com\/download\/android\" rel=\"nofollow\"\u003eTwitter for Android\u003c\/a\u003e","truncated":false,"in_reply_to_status_id":null,"in_reply_to_status_id_str":null,"in_reply_to_user_id":null,"in_reply_to_user_id_str":null,"in_reply_to_screen_name":null,"user":{"id":14766954,"id_str":"14766954","name":"Derick Bailey","screen_name":"derickbailey","location":"Waco, TX","description":"failing my way to an eventual success as an entrepreneur. @WatchMeCode @Entreprogrammer #JavaScript Opinions here are someone else\u2019s","url":"http:\/\/t.co\/eu0eCuMY85","entities":{"url":{"urls":[{"url":"http:\/\/t.co\/eu0eCuMY85","expanded_url":"http:\/\/derickbailey.com","display_url":"derickbailey.com","indices":[0,22]}]},"description":{"urls":[]}},"protected":false,"followers_count":7138,"friends_count":882,"listed_count":527,"created_at":"Wed May 14 00:32:00 +0000 2008","favourites_count":2352,"utc_offset":-18000,"time_zone":"Central Time (US & Canada)","geo_enabled":false,"verified":false,"statuses_count":50277,"lang":"en","contributors_enabled":false,"is_translator":false,"is_translation_enabled":false,"profile_background_color":"1A1B1F","profile_background_image_url":"http:\/\/abs.twimg.com\/images\/themes\/theme9\/bg.gif","profile_background_image_url_https":"https:\/\/abs.twimg.com\/images\/themes\/theme9\/bg.gif","profile_background_tile":false,"profile_image_url":"http:\/\/pbs.twimg.com\/profile_images\/574894761452003328\/AajEimnr_normal.jpeg","profile_image_url_https":"https:\/\/pbs.twimg.com\/profile_images\/574894761452003328\/AajEimnr_normal.jpeg","profile_banner_url":"https:\/\/pbs.twimg.com\/profile_banners\/14766954\/1390781300","profile_link_color":"2FC2EF","profile_sidebar_border_color":"181A1E","profile_sidebar_fill_color":"252429","profile_text_color":"666666","profile_use_background_image":true,"has_extended_profile":false,"default_profile":false,"default_profile_image":false,"following":true,"follow_request_sent":false,"notifications":false},"geo":null,"coordinates":null,"place":null,"contributors":null,"is_quote_status":false,"retweet_count":0,"favorite_count":1,"entities":{"hashtags":[{"text":"EveryDaySexism","indices":[15,30]}],"symbols":[],"user_mentions":[],"urls":[]},"favorited":false,"retweeted":false,"lang":"en"};
+module.exports = {
+  created_at: 'Tue Oct 20 13:57:55 +0000 2015',
+  id: 656469398526361600,
+  id_str: '656469398526361600',
+  text: "and in today's #EveryDaySexism, my wife can't get vehicle registration sticker because her last name is different than mine \ud83d\ude20\ud83d\ude2c",
+  source:
+    '\u003ca href="http://twitter.com/download/android" rel="nofollow"\u003eTwitter for Android\u003c/a\u003e',
+  truncated: false,
+  in_reply_to_status_id: null,
+  in_reply_to_status_id_str: null,
+  in_reply_to_user_id: null,
+  in_reply_to_user_id_str: null,
+  in_reply_to_screen_name: null,
+  user: {
+    id: 14766954,
+    id_str: '14766954',
+    name: 'Derick Bailey',
+    screen_name: 'derickbailey',
+    location: 'Waco, TX',
+    description:
+      'failing my way to an eventual success as an entrepreneur. @WatchMeCode @Entreprogrammer #JavaScript Opinions here are someone else\u2019s',
+    url: 'http://t.co/eu0eCuMY85',
+    entities: {
+      url: {
+        urls: [
+          {
+            url: 'http://t.co/eu0eCuMY85',
+            expanded_url: 'http://derickbailey.com',
+            display_url: 'derickbailey.com',
+            indices: [0, 22],
+          },
+        ],
+      },
+      description: { urls: [] },
+    },
+    protected: false,
+    followers_count: 7138,
+    friends_count: 882,
+    listed_count: 527,
+    created_at: 'Wed May 14 00:32:00 +0000 2008',
+    favourites_count: 2352,
+    utc_offset: -18000,
+    time_zone: 'Central Time (US & Canada)',
+    geo_enabled: false,
+    verified: false,
+    statuses_count: 50277,
+    lang: 'en',
+    contributors_enabled: false,
+    is_translator: false,
+    is_translation_enabled: false,
+    profile_background_color: '1A1B1F',
+    profile_background_image_url:
+      'http://abs.twimg.com/images/themes/theme9/bg.gif',
+    profile_background_image_url_https:
+      'https://abs.twimg.com/images/themes/theme9/bg.gif',
+    profile_background_tile: false,
+    profile_image_url:
+      'http://pbs.twimg.com/profile_images/574894761452003328/AajEimnr_normal.jpeg',
+    profile_image_url_https:
+      'https://pbs.twimg.com/profile_images/574894761452003328/AajEimnr_normal.jpeg',
+    profile_banner_url:
+      'https://pbs.twimg.com/profile_banners/14766954/1390781300',
+    profile_link_color: '2FC2EF',
+    profile_sidebar_border_color: '181A1E',
+    profile_sidebar_fill_color: '252429',
+    profile_text_color: '666666',
+    profile_use_background_image: true,
+    has_extended_profile: false,
+    default_profile: false,
+    default_profile_image: false,
+    following: true,
+    follow_request_sent: false,
+    notifications: false,
+  },
+  geo: null,
+  coordinates: null,
+  place: null,
+  contributors: null,
+  is_quote_status: false,
+  retweet_count: 0,
+  favorite_count: 1,
+  entities: {
+    hashtags: [{ text: 'EveryDaySexism', indices: [15, 30] }],
+    symbols: [],
+    user_mentions: [],
+    urls: [],
+  },
+  favorited: false,
+  retweeted: false,
+  lang: 'en',
+};
